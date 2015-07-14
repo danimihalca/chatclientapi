574e5c567877cd9e295c64d390542813f529809f
Daniel Mihalca <danimihalca@gmail.com>
2015-07-14 20:33:00 +0300

    Slowly moving to libwebsockets for cross compatibility with WindowsPhone

--- inc/ChatClientImpl.h
+++ inc/ChatClientImpl.h
@@ -1,12 +1,10 @@
 #ifndef CHATCLIENTIMPL_H
 #define CHATCLIENTIMPL_H
 
-#include <websocketpp/client.hpp>
-#include <websocketpp/config/asio_no_tls_client.hpp>
+#include <string>
+#include <libwebsockets.h>
 
 #include "common.h"
-
-using asioClient = websocketpp::client<websocketpp::config::asio_client>;
 
 class ChatClientImpl
 {
@@ -21,17 +19,7 @@ public:
     ~ChatClientImpl();
 
 private:
-    void setHandlers();
-    void setLogAccessChannels();
-    void onOpen(websocketpp::connection_hdl);
-    void onFail(websocketpp::connection_hdl);
-    void onClose(websocketpp::connection_hdl);
-    void onMessage(websocketpp::connection_hdl, asioClient::message_ptr msg);
-private:
-    asioClient m_client;
-    websocketpp::connection_hdl m_connectionHandle;
     bool m_connected = false;
-    std::shared_ptr<websocketpp::lib::thread> p_serviceThread;
     newMessageCallback m_newMessageCB;
 };
 
--- src/ChatClientImpl.cpp
+++ src/ChatClientImpl.cpp
@@ -6,10 +6,6 @@
 
 void ChatClientImpl::initialize()
 {
-    m_client.init_asio();
-    setHandlers();
-    setLogAccessChannels();
-    m_client.start_perpetual();
 }
 
 void ChatClientImpl::setNewMessageCallback(newMessageCallback callback)
@@ -19,106 +15,37 @@ void ChatClientImpl::setNewMessageCallback(newMessageCallback callback)
 
 void ChatClientImpl::connect(const std::string& uri)
 {
-    websocketpp::lib::error_code errorCode;
-    asioClient::connection_ptr connection = m_client.get_connection(uri, errorCode);
-    if (errorCode)
-    {
-        m_client.get_alog().write(websocketpp::log::alevel::app, "Get Connection Error: " + errorCode.message());
-        return;
-    }
-
-    m_client.connect(connection);
-    m_connectionHandle = connection->get_handle();
-    m_client.get_alog().write(websocketpp::log::alevel::app, "Connecting...");
-
 
 }
 
 void ChatClientImpl::startService()
 {
-    p_serviceThread = websocketpp::lib::make_shared<websocketpp::lib::thread>(&asioClient::run, &m_client);
 }
 
 
 
 void ChatClientImpl::sendMessage(const std::string& message)
 {
-    while (!m_connected)
-    {
-        sleep(1);
-    }
-    m_client.get_alog().write(websocketpp::log::alevel::app, "Sending Message: \"" + message +'\"');
-    websocketpp::lib::error_code errorCode;
 
-    m_client.send(m_connectionHandle, message, websocketpp::frame::opcode::text, errorCode);
-    if (errorCode)
-    {
-        m_client.get_alog().write(websocketpp::log::alevel::app, "Sending Message Error: " + errorCode.message());
-    }
 }
 
 void ChatClientImpl::closeConnection()
 {
-    m_connected = false;
-    websocketpp::lib::error_code errorCode;
-    m_client.close(m_connectionHandle,websocketpp::close::status::normal,"disconnected");
+
 }
 
 ChatClientImpl::~ChatClientImpl()
 {
-    m_client.stop_perpetual();
     if (m_connected)
     {
         closeConnection();
     }
-    if (p_serviceThread->joinable())
-    {
-        p_serviceThread->join();
-    }
+//    if (p_serviceThread->joinable())
+//    {
+//        p_serviceThread->join();
+//    }
 }
 
-void ChatClientImpl::setHandlers()
-{
-    using websocketpp::lib::placeholders::_1;
-    using websocketpp::lib::placeholders::_2;
-    using websocketpp::lib::bind;
 
-    m_client.set_open_handler(bind(&ChatClientImpl::onOpen, this, _1));
-    m_client.set_close_handler(bind(&ChatClientImpl::onClose, this, _1));
-    m_client.set_fail_handler(bind(&ChatClientImpl::onFail, this, _1));
-    m_client.set_message_handler(bind(&ChatClientImpl::onMessage, this, _1, _2));
-}
 
-void ChatClientImpl::setLogAccessChannels()
-{
-    m_client.clear_access_channels(websocketpp::log::alevel::all);
-    m_client.set_access_channels(websocketpp::log::alevel::connect);
-    m_client.set_access_channels(websocketpp::log::alevel::disconnect);
-    m_client.set_access_channels(websocketpp::log::alevel::app);
-}
 
-void ChatClientImpl::onOpen(websocketpp::connection_hdl)
-{
-    m_client.get_alog().write(websocketpp::log::alevel::app, "Connection opened!");
-    m_connected = true;
-}
-
-void ChatClientImpl::onFail(websocketpp::connection_hdl)
-{
-    m_client.get_alog().write(websocketpp::log::alevel::app, "Connection failed!");
-}
-
-void ChatClientImpl::onClose(websocketpp::connection_hdl)
-{
-    m_client.get_alog().write(websocketpp::log::alevel::app, "Connection closed!");
-    m_connected = false;
-}
-
-void ChatClientImpl::onMessage(websocketpp::connection_hdl,asioClient::message_ptr msg)
-{
-    m_client.get_alog().write(websocketpp::log::alevel::app, "Message received!!");
-    std::string message = msg->get_payload();
-    m_client.get_alog().write(websocketpp::log::alevel::app, "Message:" + message);
-//    std::cout<<"MESSAGE: "<<msg->get_payload()<<std::endl;
-    m_newMessageCB(message.c_str());
-}
